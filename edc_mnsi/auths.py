fb40831fdce11121dd7236a9c7fc577220c2e00e
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2021-09-17 17:55:31 +0100

    Add get_mnsi_model_name/cls, and use in auths/auth_objects

--- edc_mnsi/auths.py
+++ edc_mnsi/auths.py
@@ -1,31 +1,8 @@
 from edc_auth.site_auths import site_auths
 
-from .auth_objects import MNSI, MNSI_EXPORT, MNSI_SUPER, MNSI_VIEW
+from .auth_objects import MNSI, MNSI_EXPORT, MNSI_SUPER, MNSI_VIEW, mnsi_codenames
 
-site_auths.add_group(
-    "edc_mnsi.view_mnsi",
-    "edc_mnsi.view_historicalmnsi",
-    name=MNSI_VIEW,
-)
-
-site_auths.add_group(
-    "edc_mnsi.add_mnsi",
-    "edc_mnsi.change_mnsi",
-    "edc_mnsi.view_mnsi",
-    "edc_mnsi.view_historicalmnsi",
-    name=MNSI,
-)
-
-site_auths.add_group(
-    "edc_mnsi.delete_mnsi",
-    "edc_mnsi.add_mnsi",
-    "edc_mnsi.change_mnsi",
-    "edc_mnsi.view_mnsi",
-    "edc_mnsi.view_historicalmnsi",
-    name=MNSI_SUPER,
-)
-
-site_auths.add_group(
-    "edc_mnsi.export_mnsi",
-    name=MNSI_EXPORT,
-)
+site_auths.add_group(*mnsi_codenames, name=MNSI_VIEW, view_only=True)
+site_auths.add_group(*mnsi_codenames, name=MNSI, no_delete=True)
+site_auths.add_group(*mnsi_codenames, name=MNSI_SUPER)
+site_auths.add_group(*mnsi_codenames, name=MNSI_EXPORT, convert_to_export=True)
--- edc_mnsi/utils.py
+++ edc_mnsi/utils.py
@@ -3,6 +3,14 @@ from django.conf import settings
 from django.db import models
 
 
+def get_mnsi_model_name() -> str:
+    return getattr(settings, "EDC_MNSI_MODEL", "edc_mnsi.mnsi")
+
+
+def get_mnsi_model_cls() -> models.Model:
+    return django_apps.get_model(get_mnsi_model_name())
+
+
 def get_abnormal_foot_appearance_obs_model_name() -> str:
     return getattr(
         settings,
