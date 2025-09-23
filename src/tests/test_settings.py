aa4ee312ff315e1e9c2e5335b5eed38628b26ea0
erikvw <ew2789@gmail.com>
2025-09-23 17:13:28 -0500

    test config

--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,65 +1,27 @@
-#!/usr/bin/env python
 import sys
 from pathlib import Path
 
-from edc_test_settings.default_test_settings import DefaultTestSettings
+from clinicedc_tests.config import DefaultTestSettings, get_installed_apps_for_tests
 
 app_name = "edc_mnsi"
-
 base_dir = Path(__file__).absolute().parent.parent.parent
 
 project_settings = DefaultTestSettings(
     calling_file=__file__,
     BASE_DIR=base_dir,
     APP_NAME=app_name,
-    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
     SILENCED_SYSTEM_CHECKS=[
         "sites.E101",
-        "edc_navbar.E002",
-        "edc_navbar.E003",
-        "edc_consent.E001",
         "edc_sites.E001",
         "edc_sites.E002",
-    ],
-    EDC_AUTH_SKIP_SITE_AUTHS=True,
-    EDC_AUTH_SKIP_AUTH_UPDATER=True,
-    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
-    EDC_SITES_REGISTER_DEFAULT=True,
+        "edc_navbar.E003",
+        "edc_consent.E001",
+    ],  # The SITE_ID setting must be an integer
     INSTALLED_APPS=[
-        "django.contrib.admin",
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sessions",
-        "django.contrib.messages",
-        "django.contrib.staticfiles",
-        "django.contrib.sites",
-        "multisite",
-        "django_crypto_fields.apps.AppConfig",
-        "django_revision.apps.AppConfig",
-        "edc_action_item.apps.AppConfig",
-        "edc_appointment.apps.AppConfig",
-        "edc_adverse_event.apps.AppConfig",
-        "edc_auth.apps.AppConfig",
-        "edc_dashboard.apps.AppConfig",
-        "edc_data_manager.apps.AppConfig",
-        "edc_facility.apps.AppConfig",
-        "edc_form_runners.apps.AppConfig",
-        "edc_lab.apps.AppConfig",
-        "edc_list_data.apps.AppConfig",
-        "edc_listboard.apps.AppConfig",
-        "edc_metadata.apps.AppConfig",
-        "edc_navbar.apps.AppConfig",
-        "edc_notification.apps.AppConfig",
-        "edc_registration.apps.AppConfig",
-        "edc_review_dashboard.apps.AppConfig",
-        "edc_sites.apps.AppConfig",
-        "edc_subject_dashboard.apps.AppConfig",
-        "edc_visit_schedule.apps.AppConfig",
-        "edc_visit_tracking.apps.AppConfig",
-        "edc_mnsi.apps.AppConfig",
-        "edc_appconfig.apps.AppConfig",
+        *get_installed_apps_for_tests(f"{app_name}.apps.AppConfig"),
     ],
     add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
 ).settings
 
 for k, v in project_settings.items():
